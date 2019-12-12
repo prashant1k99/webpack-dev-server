e2e95a6fa641b8d9a0215807eaa869b99bf7c9cd
Prashant Singh <prashantco111@gmail.com>
2019-12-12 17:02:27 +0530

    Basic Setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            query: {
+              presets: ['@babel/preset-env'],
+            },
+          }
+        ]
+      }
+    ]
+  },
+  entry: {
+    app : './src/index.js'
+  }
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    publicPath: '/',
+    filename: 'build.js',
+    library: 'Tags',
+    libraryTarget: 'umd',
+    libraryExport: 'default'
+  }
+};
